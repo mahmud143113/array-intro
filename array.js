ac7555f3b28ad951b09e41ecce416392ba3a4bb7
mahmud143113 <jm1431013@gmail.com>
2023-02-18 14:50:32 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,13 @@
+// var todayClassDate = 18;
+// var  className = "Javascript Array";
+
+// var mySchoolName = "Mosjid Mission Academy";
+
+// // console.log(className);
+
+var friend = "Mahmud";
+how to remove & add name on first line
+
+
+// HW
+specific vabe kmne add kore & remove kore
